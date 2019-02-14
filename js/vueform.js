befc22c047e8aed1d307c036b9fc3be8820893a2
Michael Barnett <michaelbrettbarnett@gmail.com>
2019-02-14 20:38:49 +1100

    Started base form. Adding question info as its tested. Also added To Do list.

---
+++ js/vueform.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        current_page: 1,
+        questions: [{
+            question_name: "Store Type?",
+            question_value: null,
+            question_required: true,
+            question_page: 1,
+            question_type: "radio",
+            vals: [{
+                val_name: "Mall",
+                val_value: 1
+            },{
+                val_name: "Metro",
+                val_value: 2
+            },{
+                val_name: "Arcade",
+                val_value: 3
+            }, {
+                val_name: "Centre",
+                val_value: 4
+            }],
+            extent_questions: [{
+                eval: 2,
+                question_name: "Please provide details about the role",
+                question_value: null,
+                question_required: true,
+                question_type: "textarea"
+            }]}, {
+                
+            }
+
+
+        }]
+    }
+
+
+
+
+
+
+
+
+
+})
